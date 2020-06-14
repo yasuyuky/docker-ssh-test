92014a4856f4fee686b80b97d62edafc4713fd4b
yasuyuky <yasuyuki.ymd@gmail.com>
2020-06-14 15:27:09 +0900

    Define whole matrix

--- build_local.bash
+++ build_local.bash
@@ -1,4 +1,4 @@
-for d in $(jq -r '.[]|.dist+"."+.ver' dist.json); do
+for d in $(jq -r '.include[]|.dist+"."+.ver' dist.json); do
     echo ${d%.*} ${d#*.}
     docker build . --build-arg dist=${d%.*} --build-arg ver=${d#*.} -t yasuyuky/ssh-test:${d}
 done
